70c131d74a3df285a70e506196d4928f69874de7
drewburns <drewburnsbab@gmail.com>
2021-01-09 23:39:54 -0500

    v1

--- browser_action.js
+++ browser_action.js
@@ -1,12 +1,75 @@
-// function onlyUnique(value, index, self) { 
+// function onlyUnique(value, index, self) {
 //     return self.indexOf(value) === index;
 // }
+// alert("yolo")
+// $('#cookieCount').text("TEST");
+// chrome.storage.sync.set({ linkList: [] }, function () {});
 
-chrome.storage.sync.get(["cookieList"], function (items) {
-    console.log(items);
-    try {
-        $('#cookieCount').text(items.cookieList.length);
-    }catch(err) {
-        $('#cookieCount').text("0");
-    }
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+chrome.storage.sync.get(["linkList"], function (items) {
+  try {
+    // load links
+    $("#newLink").click(() => {
+      chrome.storage.sync.get(["linkList"], function (items2) {
+        const nameInput = $("#newName");
+        const urlInput = $("#newURL");
+
+        const newId = makeid(7);
+        const newLink = {
+          id: newId,
+          name: nameInput.val(),
+          url: urlInput.val(),
+        };
+        const newList = items2.linkList
+          ? items2.linkList.concat(newLink)
+          : [newLink];
+        chrome.storage.sync.set({ linkList: newList }, function () {
+          $("#link-list").prepend(
+            `<li id=${newId}><h5><b>${nameInput.val()}</b>: ${urlInput.val()}</h5><div id=${
+              "del_" + newId
+            } type="button" class="btn btn-danger">Delete</div></li>`
+          );
+          $(`#del_${newId}`).click(() => {
+            removeLink(newId);
+          });
+          nameInput.val("");
+          urlInput.val("");
+        });
+      });
+
+      //   alert("heelo!");
+    });
+    items.linkList.forEach((item) => {
+      $("#link-list").append(
+        `<li id=${item.id}><h5><b>${item.name}</b>: ${item.url}</h5> <button id=${
+          "del_" + item.id
+        } type="button" class="btn btn-danger" >Delete</button></li>`
+      );
+
+      $(`#del_${item.id}`).click(() => {
+        removeLink(item.id);
+        // alert(`clicked: ${item.id}`);
+      });
+    });
+  } catch (err) {
+    // alert(err);
+  }
 });
+
+const removeLink = (id) => {
+  chrome.storage.sync.get(["linkList"], function (items) {
+    const newItems = items.linkList.filter((i) => i.id !== id);
+    chrome.storage.sync.set({ linkList: newItems }, function () {});
+    $(`#${id}`).remove();
+  });
+};
